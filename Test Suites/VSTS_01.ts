679ca1b79292b0d267d5bb20adcb8a5e3d0ae89d
mamidisrisaiteja@gmail.com <mamidisrisaiteja@gmail.com>
2024-04-28 12:18:41 +0530

    Commit _vs_01

---
+++ Test Suites/VSTS_01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VSTS_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4c30f52a-159c-4598-8fab-7493972781ed</testSuiteGuid>
+</TestSuiteEntity>
