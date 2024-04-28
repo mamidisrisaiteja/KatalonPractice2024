66dd31a5fd41a60ceb3b0126acb7ff8c9c251e6d
mamidisrisaiteja@gmail.com <mamidisrisaiteja@gmail.com>
2024-04-28 11:32:10 +0530

    Commit02

---
+++ Test Suites/PetStoreTS02.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PetStoreTS02</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d9751f12-93d4-4bcd-a3c5-c915e83ff8d3</testSuiteGuid>
+   <testCaseLink>
+      <guid>056ed515-b804-4601-b999-725259146d11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PetStore/Login02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
